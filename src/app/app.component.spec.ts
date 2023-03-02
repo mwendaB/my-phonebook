77a5833a8feae3c9160aa1beb990848752c25266
mwendaB <brianmwenda255@gmail.com>
2023-03-02 09:41:38 +0300

    add the main imports from the components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-phonebook'`, () => {
+  it(`should have as title 'phone-book'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-phonebook');
+    expect(app.title).toEqual('phone-book');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-phonebook app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('phone-book app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
+import { AddContactComponent } from './components/add-contact/add-contact.component';
+import { ViewContactComponent } from './components/view-contact/view-contact.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
+import { EditContactComponent } from './components/edit-contact/edit-contact.component';
+import { ContactManagerComponent } from './components/contact-manager/contact-manager.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+import {HttpClientModule} from "@angular/common/http"
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    NavbarComponent,
+    ContactManagerComponent,
+    EditContactComponent,
+    SpinnerComponent,
+    ViewContactComponent,
+    AddContactComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
